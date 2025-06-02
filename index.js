33aa198a6e1c6af5eed491744caf7f2b07c5bf67
CGSG220AI3 <cgsg52ai3@gmail.com>
2025-06-02 21:52:49 +0300

    try to fix server again

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ app.all(/.+\.php$/, php.router);
 app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
+
+var sass = require('node-sass');
+const { styleText } = require('util');
+sass.render({
+  file: 'src/styles/style.sass',
+}, function(err, result) { /*...*/ });
