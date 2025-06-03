ed6636ae842031a4f4cdc4a57b8914c0ebcc0289
CGSG220AI3 <cgsg52ai3@gmail.com>
2025-06-03 22:11:44 +0300

    todo list added

---
+++ TODO LIST/todo.js
@@ -0,0 +1,4 @@
+// todo #11 @CGSG220AI3 posibility to create more shaders
+// todo #10 @CGSG220AI3 posibility to create more buffers
+// todo #9 @CGSG220AI3 create vertex type
+// todo #12 @CGSG220AI3 create compute shader
