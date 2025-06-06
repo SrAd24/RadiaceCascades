e730d9f40df0d93249a1435a25636237a53a7000
CGSG220AI3 <cgsg52ai3@gmail.com>
2025-06-06 14:16:52 +0000

    favicon

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Radiance cascades forever</title>
     <link rel="stylesheet" href="./src/styles/css/style.css">
+    <link rel="icon" href="./bin/favicon/favicon.png" type="image/png">
+    <link rel="shortcut icon" href="./bin/favicon/favicon.png" type="image/png">
 </head>
 <body id="body">
     <canvas id = "The_only_normal_group_for_the_entire_time_at_the_CGSG" width = "800" height = "600">
